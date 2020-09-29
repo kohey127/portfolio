71cdf9ee87f5dc77686916ec7a02f7eb0dc42c78
kohey127 <koooochoki@gmail.com>
2020-09-29 22:53:05 +0900

    migationファイルの順番修正

--- db/migrate/20200928040300_create_appointments.rb
+++ db/migrate/20200928040300_create_appointments.rb
@@ -2,7 +2,6 @@ class CreateAppointments < ActiveRecord::Migration[5.2]
   def change
     create_table :appointments do |t|
       t.references :customer, index: false, foreign_key: true
-      t.references :service, index: false, foreign_key: true
       t.integer	:from_customer_id, null: false
       t.integer	:request_format, null: false
       t.integer	:request_date, null: false
