a3b790b172d349ee151b4a0724e169ebfeaf4b34
kohey127 <koooochoki@gmail.com>
2020-10-02 01:02:52 +0900

    マイページ編集画面表示 Closes #25

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,22 +3,23 @@ class Public::CustomersController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
-
+    @genres = Genre.all
   end
 
   def update
-
   end
 
   def unsubscribe
-
   end
 
   def withdraw
-    
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:image, :name, :introduction, :based)
   end
 end
