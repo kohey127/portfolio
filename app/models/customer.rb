ebbc670162e4ab62f9abd6a5255a11ece97d9af6
kohey127 <koooochoki@gmail.com>
2020-10-07 21:37:11 +0900

    管理者画面表示

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,10 +6,10 @@ class Customer < ApplicationRecord
 
   has_many :services, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :contacts, dependent: :destroy
+  # has_many :contacts, dependent: :destroy
   has_many :appointment_comments, dependent: :destroy
-  has_many :customer_genres, dependent: :destroy
-  has_many :favorites, dependent: :destroy
+  # has_many :customer_genres, dependent: :destroy
+  # has_many :favorites, dependent: :destroy
   has_many :point_histories, dependent: :destroy
   has_many :exp_histories, dependent: :destroy
 
