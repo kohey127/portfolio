55ab3b4279dbf4e1f8cfc35c26a06e5ee1e37cc7
kohey127 <koooochoki@gmail.com>
2020-10-18 17:06:43 +0900

    コメントテーブルに口コミ評価用カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_075358) do
+ActiveRecord::Schema.define(version: 2020_10_18_080415) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_075358) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "customers", force: :cascade do |t|
