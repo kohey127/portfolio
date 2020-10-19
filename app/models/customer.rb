8caff092f5ce53525796220d716d474a516f0ce2
kohey127 <koooochoki@gmail.com>
2020-10-19 15:34:02 +0900

    顧客のポジティブ評価画面デザイン追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -30,19 +30,22 @@ class Customer < ApplicationRecord
     Comment.joins(service: :customer).where(customers: {id: self.id})
   end
 
-  # 顧客が受けたレビューの平均を百分率で取得
+  # 顧客が受けたレビューのポジティブ具合(Natural Language)の平均を百分率で取得
   def get_raty
     # sum_ratyでレビューの合計を取得
     sum_raty = 0
     # count_ratyでレビューの件数を取得
     count_raty = 0
-    # 顧客が受けたレビューの中で、レビューがあるものだけを配列で取得
+    # 顧客が受けたレビューの中でNatural Languageのデータが存在するものだけを配列で取得
     Comment.joins(service: :customer).where(customers: {id: self.id}).pluck(:score).compact.each do |raty|
       sum_raty += raty
       count_raty += 1
     end
-    # レビューの平均値を百分率で取得
-    sum_raty / count_raty * 100
-    # get_comments.all.sum(:score) / get_comments.count * 100
+    if count_raty != 0 
+      # レビューの平均値を百分率で取得
+      ( sum_raty / count_raty * 100 ).round
+    else
+      0
+    end
   end
 end
